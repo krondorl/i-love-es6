a0d6e5b0e9a4a69fb605053502ecb3ab0707ec8f
Adam Burucs <adam.burucs@insdata.local>
2018-01-05 16:30:34 +0100

    Add Arrow Function practice

---
+++ arrow-functions.js
@@ -0,0 +1,82 @@
+// Arrow functions practice
+
+var add = (a, b) => {
+  return a + b;
+};
+
+var subtract = (a, b) => {
+  return a - b;
+};
+
+var multiply = (a, b) => {
+  return a * b;
+};
+
+var divide = (a, b) => {
+  return a / b;
+};
+
+var isOdd = (a) => {
+  return (a % 2 === 0) ? 'Not an odd number' : 'Odd number';
+};
+
+var isEven = (a) => {
+  return (a % 2 === 0) ? 'Even number' : 'Not an even number';
+};
+
+var isPrime = (a) => {
+  for (let i = 2; i < a; i++) {
+    if (a % i === 0) {
+      return false;
+    }
+  }
+  return a !== 1;
+}
+
+var isPalindrome = (str) => {
+  return str == str.split('').reverse().join('');
+}
+
+var lengthCheck = (str) => {
+  return str.length;
+}
+
+var dog = () => {
+  return 'Bark!';
+}
+
+var cat = () => {
+  return 'Meow...';
+}
+
+var pig = () => {
+  return 'Oink, oink.';
+}
+
+var duck = () => {
+  return 'Quack!';
+}
+
+console.log('>add(1,2)');
+console.log(add(1,2));
+console.log('>subtract(5,6)');
+console.log(subtract(5,6));
+console.log('>multiply(7,7)');
+console.log(multiply(7,7));
+console.log('>divide(121,7)');
+console.log(divide(121,7));
+console.log('>isOdd(1)');
+console.log(isOdd(1));
+console.log('>isEven(8)');
+console.log(isEven(8));
+console.log('>isPrime(17)');
+console.log(isPrime(17));
+console.log(`>isPalindrome('29')`);
+console.log(isPalindrome('29'));
+console.log(`>lengthCheck('apple juice')`);
+console.log(lengthCheck('apple juice'));
+console.log('>Animal sounds');
+console.log(dog());
+console.log(cat());
+console.log(pig());
+console.log(duck());
