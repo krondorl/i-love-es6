63e8bcfc536e3ccbc47e002a2d80f6c1b626c410
Adam Burucs <adam.burucs@insdata.local>
2018-01-08 15:13:12 +0100

    Add Let Const

---
+++ let-const.js
@@ -0,0 +1,28 @@
+const add_three = (n) => {
+  let add_num = 3;
+  return n + add_num;
+}
+
+const add_four = (n) => {
+  let add_num = 4;
+  return n + add_num;
+}
+
+const add_five = (n) => {
+  let add_num = 5;
+  return n + add_num;
+}
+
+const add_six = (n) => {
+  let add_num = 6;
+  return n + add_num;
+}
+
+console.log('add_three(1)');
+console.log(add_three(1));
+console.log('add_four(2)');
+console.log(add_four(2));
+console.log('add_five(3)');
+console.log(add_five(3));
+console.log('add_six(4)');
+console.log(add_six(4));
