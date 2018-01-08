3081146f9c7088c1102508b81468f5d10fce431d
Adam Burucs <adam.burucs@insdata.local>
2018-01-08 12:49:17 +0100

    Add Template Literals

---
+++ template-literals.js
@@ -0,0 +1,28 @@
+var fr1 = `apple`;
+var fr2 = `pear`;
+var fr3 = `orange`;
+var fruits = `I like to eat these: ${fr1}, ${fr2}, ${fr3}`;
+
+var vg1 = `broccoli`;
+var vg2 = `tomato`;
+var vg3 = `carrot`;
+var veggies = `I like to eat these as well: ${vg1}, ${vg2}, ${vg3}`;
+
+var pg1 = `HTML5`;
+var pg2 = `CSS3`;
+var pg3 = `JavaScript ES6`;
+var programming = `I use these programming languages: ${pg1}, ${pg2}, ${pg3}`;
+
+var pet = `dog`;
+var pets = `I have got a pet: ${pet}`;
+
+var c1 = `white`;
+var c2 = `blue`;
+var c3 = `green`;
+var colors = `My favorite colors are: ${c1}, ${c2}, ${c3}`;
+
+console.log(fruits);
+console.log(veggies);
+console.log(programming);
+console.log(pets);
+console.log(colors);
