8219b5857b6d701c1c2906b5206645632f3f1143
Adam Burucs <adam.burucs@insdata.local>
2018-01-08 16:16:54 +0100

    Add Repeat

---
+++ repeat.js
@@ -0,0 +1,8 @@
+console.log(`"apple".repeat(3)`);
+console.log("apple".repeat(3));
+console.log(`"grape".repeat(6)`);
+console.log("grape".repeat(6));
+console.log(`"mate".repeat(10)`);
+console.log("mate".repeat(10));
+console.log(`"sail-".repeat(8)`);
+console.log("sail-".repeat(8));
