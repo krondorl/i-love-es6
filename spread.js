8931d193f82be0fe3e8ac2542a45ad0519889f2b
Adam Burucs <adam.burucs@insdata.local>
2018-01-08 15:12:59 +0100

    Add Spread

---
+++ spread.js
@@ -0,0 +1,15 @@
+var arr = ['apple', 'pear', 'plum'];
+var arr2 = [...arr, 'banana'];
+
+console.log('arr');
+console.log(arr);
+console.log('arr2');
+console.log(arr2);
+
+var animal1 = ['dog', 'cat', 'mouse'];
+var animal2 = [...arr, 'horse'];
+
+console.log('animal1');
+console.log(animal1);
+console.log('animal2');
+console.log(animal2);
