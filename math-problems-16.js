5e5f53111da8b574d58d5ada4445c4df805ce63c
Adam Burucs <adam.burucs@insdata.local>
2018-01-10 16:08:45 +0100

    Add Math Problem 16

---
+++ math-problems-16.js
@@ -0,0 +1,21 @@
+// Problem 16
+// Power digit sum
+// 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+//
+// What is the sum of the digits of the number 21000?
+
+const bigInt = require("big-integer");
+
+const powerNumber = bigInt(2).pow(1000).toString();
+
+const calculatePowerNumber = () => {
+  var powerDigitSum = 0;
+
+  powerNumber.split('').map((letter) => {
+    powerDigitSum += parseInt(letter);
+  });
+  return powerDigitSum;
+}
+
+console.log(`calculatePowerNumber()`);
+console.log(calculatePowerNumber());
